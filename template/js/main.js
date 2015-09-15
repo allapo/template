c0cf9e9375e547efce4b5392fa26bddcc8339184
allapo <kahfimuhammad01@gmail.com>
2015-09-15 15:11:56 +0700

    template

---
+++ template/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+ $(document).ready(function(){
+    $('.tooltipped').tooltip({delay: 50});
+  });
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+  });
+$(document).ready(function(){
+    $('ul.tabs').tabs('select_tab', 'tab_id');
+  });
+$(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
